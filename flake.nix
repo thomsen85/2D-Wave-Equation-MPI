6ef3c2a3cbb8fbe11b0f0a55df4a14ea24cf1b6c
Thomas Svendal <thomassvendal@gmail.com>
2024-10-03 18:21:10 +0200

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A basic dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              gcc
+              gdb
+              openmpi
+
+              ffmpeg
+              gnuplot
+              clang-tools
+            ];
+          };
+
+          LD_LIBRARY_PATH = lib.makeLibraryPath [
+            openmpi
+            clang-tools
+          ];
+        }
+    );
+}
